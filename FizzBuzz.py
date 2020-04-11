cdb681cff26110dc911efb5bdd201d7075f6303d
Dina Ahmed <da2kb@mail.missouri.edu>
2020-04-10 22:47:55 -0500

    a

---
+++ FizzBuzz.py
@@ -0,0 +1,131 @@
+Python 3.8.2 (v3.8.2:7b3ab5921f, Feb 24 2020, 17:52:18) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print(Dina)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+
+>>> print("Dina")
+Dina
+>>> for fizzbuzz in range(100):  
+  
+
+    if fizzbuzz % 15 == 0:  
+        print("FizzBuzz")                                          
+        continue
+  
+
+    elif fizzbuzz % 3 == 0:      
+        print("Fizz")                                          
+        continue
+  
+
+    elif fizzbuzz % 5 == 0:          
+        print("Buzz")                                      
+        continue
+
+    print(fizzbuzz)
+
+    
+FizzBuzz
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+FizzBuzz
+31
+32
+Fizz
+34
+Buzz
+Fizz
+37
+38
+Fizz
+Buzz
+41
+Fizz
+43
+44
+FizzBuzz
+46
+47
+Fizz
+49
+Buzz
+Fizz
+52
+53
+Fizz
+Buzz
+56
+Fizz
+58
+59
+FizzBuzz
+61
+62
+Fizz
+64
+Buzz
+Fizz
+67
+68
+Fizz
+Buzz
+71
+Fizz
+73
+74
+FizzBuzz
+76
+77
+Fizz
+79
+Buzz
+Fizz
+82
+83
+Fizz
+Buzz
+86
+Fizz
+88
+89
+FizzBuzz
+91
+92
+Fizz
+94
+Buzz
+Fizz
+97
+98
+Fizz
+>>> 
